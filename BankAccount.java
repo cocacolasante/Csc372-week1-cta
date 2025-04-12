4ac4583063779ef7a9a0762127bfd62f8c00f5af
cocacolasante <105400476+cocacolasante@users.noreply.github.com>
2025-04-12 13:36:55 -0400

    updated

--- BankAccount.java
+++ BankAccount.java
@@ -1,73 +1,82 @@
-
-
 public class BankAccount {
-    // Fields:
 
-// string firstName
-private String firstName;
-private String lastName;
-private int accountID;
-protected double balance;
+    private String firstName;
+    private String lastName;
+    private int accountId;
+    protected double balance;
 
-// constructor():  initialize balance to zero
-// deposit() - will accept a single value double parameter; the parameter value is added to the existing balance
-// withdrawal() - accepts a single value double dollar amount; the parameter value is subtracted from the existing balance
-// Setters and getters for firstName, lastName, and accountID
-// getBalance() getter to return the balance
-// accountSummary() - prints all account information
-
-public BankAccount(String _firstName, String _lastName, int _accountId){
-    this.firstName = _firstName;
-    this.lastName = _lastName;
-    this.accountID = _accountId;
-    this.balance = 0.0;
-}
-
-public void deposit(double amount){
-    if(amount >0.0 ){
-        this.balance += amount;
+    /**
+     * @param firstName owners first name
+     * @param lastName  owners last name
+     * @param accountId unique account identifier
+     */
+    public BankAccount(String firstName, String lastName, int accountId) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.accountId = accountId;
+        this.balance = 0.0;
     }
-}
 
-public void withdrawal(double amount){
-    if(this.balance - amount >=0 ){
-        this.balance -= amount;
+    /**
+     * @param amount the amount to deposit;
+     * @throws IllegalArgumentException if amount <= 0
+     */
+    public void deposit(double amount) {
+        validatePositiveAmount(amount);
+        balance += amount;
     }
-}
 
-public void accountSummary() {
-    System.out.println("name: " + firstName + " " + lastName);
-    System.out.println("ID: " + accountID);
-    System.out.println("balance:$" + balance);
-}
+    /**
+     * @param amount the amount to withdraw; 
+     * @throws IllegalArgumentException if amount <= 0 or insufficient funds
+     */
+    public void withdrawal(double amount) {
+        validatePositiveAmount(amount);
+        if (amount > balance) {
+            throw new IllegalArgumentException("Insufficient funds");
+        }
+        balance -= amount;
+    }
 
-public String getFirstName() {
-    return firstName;
-}
+    /**
+     * prints a summary of the account to System.out.
+     */
+    public void accountSummary() {
+        System.out.printf("Name: %s %s%nID: %d%nBalance: $%.2f%n",
+                firstName, lastName, accountId, balance);
+    }
 
-public void setFirstName(String firstName) {
-    this.firstName = firstName;
-}
+    public String getFirstName() {
+        return firstName;
+    }
 
-public String getLastName() {
-    return lastName;
-}
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-public void setLastName(String lastName) {
-    this.lastName = lastName;
-}
+    public String getLastName() {
+        return lastName;
+    }
 
-public int getAccountID() {
-    return accountID;
-}
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-public void setAccountID(int accountID) {
-    this.accountID = accountID;
-}
+    public int getAccountId() {
+        return accountId;
+    }
 
-public double getBalance() {
-    return balance;
-}
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
+    }
 
+    public double getBalance() {
+        return balance;
+    }
 
+    private void validatePositiveAmount(double amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Amount must be positive");
+        }
+    }
 }
--- CheckingAccount.java
+++ CheckingAccount.java
@@ -1,31 +1,53 @@
-public class CheckingAccount extends BankAccount{
+
+
+public class CheckingAccount extends BankAccount {
+
+    private static final double OVERDRAFT_FEE = 30.0;
     private double interestRate;
 
-
-    public CheckingAccount(String firstName, String lastName, int accountID, double interestRate) {
-        super(firstName, lastName, accountID);
+    /**
+     * Constructs a new {@code CheckingAccount}.
+     *
+     * @param firstName    owners first name
+     * @param lastName     owners last name
+     * @param accountId    unique account identifier
+     * @param interestRate annual interest rate 
+     */
+    public CheckingAccount(String firstName, String lastName, int accountId, double interestRate) {
+        super(firstName, lastName, accountId);
         this.interestRate = interestRate;
     }
 
-    public void processWithdrawal(double amount){
-        if( amount <this.getBalance()){
-            withdrawal(amount);
-            System.out.println(getFirstName() +"sucessfully withdrew: " + amount);
-        } else{
-            double overdraftFee = 30.0;
+    /**
 
-            this.balance = balance - amount - overdraftFee;
-            System.out.println(getFirstName() + "has overdrawn the account");
-            System.out.println("New balance (negative): $" + balance);
+     * if amount  is greater than the current balance, the account is
+     * overdrawn and an overdraft fee is applied.
+     *
+     * @param amount amount to withdraw
+     * @throws IllegalArgumentException if amount <= 0
+     */
+    public void processWithdrawal(double amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Amount must be positive");
+        }
+
+        if (amount <= getBalance()) {
+            withdrawal(amount);
+            System.out.printf("%s successfully withdrew: $%.2f%n", getFirstName(), amount);
+        } else {
+            balance = balance - amount - OVERDRAFT_FEE;
+            System.out.printf("%s has overdrawn the account%nNew balance (negative): $%.2f%n",
+                    getFirstName(), getBalance());
         }
     }
 
+    /**
+     * displays account details with interest rate.
+     */
     public void displayAccount() {
         System.out.println("Checking account details:");
-        System.out.println("name: " + getFirstName() + " " + getLastName());
-        System.out.println("ID: " + getAccountID());
-        System.out.println("balance: $" + getBalance());
-        System.out.println("interest rate: " + interestRate);
+        accountSummary();
+        System.out.printf("Interest rate: %.2f%%%n", interestRate * 100);
     }
 
     public double getInterestRate() {
